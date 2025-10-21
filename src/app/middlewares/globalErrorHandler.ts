6de3ab2dcda9cd4e23889bed91da0da12df9d20e
md.munna <mk8761174@gmail.com>
2025-10-21 09:13:49 +0600

    60-2 Prisma Error Handling – Part 2

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,33 +1,52 @@
 import { Prisma } from "@prisma/client";
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code == "P2002") {
+      (message = "Duplicate key error"),
+        (error = error.meta),
+        (statusCode = httpStatus.CONFLICT);
+    }
+    if (error.code == "P1000") {
+      (message = "Authentication failed against database server!"),
+        (error = error.meta),
+        (statusCode = httpStatus.BAD_GATEWAY);
+    }
+    if (error.code == "P2003") {
+      (message = "Foreign key constraint failed on the field!"),
+        (error = error.meta),
+        (statusCode = httpStatus.BAD_REQUEST);
+    }
+  } else if (error instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    (error = error.message), (statusCode = httpStatus.BAD_REQUEST);
+  } else if (error instanceof Prisma.PrismaClientUnknownRequestError) {
+    message = "Something Unknown Error Happened!";
+    error = error.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  } else if (error instanceof Prisma.PrismaClientInitializationError) {
+    message = "Prisma Client Failed To Initialize!";
+    error = error.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  }
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    let success = false;
-    let message = err.message || "Something went wrong!";
-    let error = err;
-if(err instanceof Prisma.PrismaClientKnownRequestError){
-    if(err.code === "P2002"){
-        message = "Duplicate key error",
-        error = err.meta
-    }
-    if(err.code === "P1000"){
-        message = "Authentication failed against database server",
-        error = err.meta
-    }
-    if(err.code === "P2003"){
-        message = "foreign key constraint failed",
-        error = err.meta
-    }
-}
-    res.status(statusCode).json({
-        success,
-        message,
-        error
-    })
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
 };
 
 export default globalErrorHandler;
